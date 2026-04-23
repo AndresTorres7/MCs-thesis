77fb6374d71b51c94680fd5cda4fc5550c94a43c
AndresTorres7 <98290511+AndresTorres7@users.noreply.github.com>
2026-04-23 07:50:23 -0500

    Add files via upload

--- geographicmode/inferecegeographicmode.R
+++ geographicmode/inferecegeographicmode.R
@@ -16,6 +16,9 @@ library(rgbif)
 library(readxl)
 library(terra)
 library(RANN)
+library(ggpubr)
+library(tidyverse)
+library(dplyr)
 
 scripts <- c("rangeDispersal.R", "nicheEvolution.R","speciateAllopatric.R","speciateSympatric.R",
              "speciateParapatric.R","speciateDispersal.R","seedSpecies.R","environmentalChange.R",
@@ -24,7 +27,7 @@ scripts <- c("rangeDispersal.R", "nicheEvolution.R","speciateAllopatric.R","spec
 
 
 for (i in 1:length(scripts)){
-  scripts[[i]] <- paste("~/Biologia/diversificationTest/Code/AuxiliaryScripts/DREAD/",scripts[[i]],sep="")
+  scripts[[i]] <- paste("DREAD/",scripts[[i]],sep="")
 }
 lapply(scripts, source)
 
@@ -76,7 +79,7 @@ df <- data.frame(clade = NA, ntips = NA, crown.age=NA,
 
 results_df <- data.frame()
 
-for (w in 1:length(newclade.trees)){
+for (w in 76:length(newclade.trees)){
   phy = newclade.trees[[w]]
   Clade= names(newclade.trees)[w]
   
@@ -99,7 +102,8 @@ for (w in 1:length(newclade.trees)){
   
   # get sister species range distances
   print(paste("running range distance ... takes a min"))
-  #trace("sisterSpeciesRangeDistances", edit = T)  # 3 times min(pointDistance(p1, p2, longlat=F) to min(nn2(data = p2, query = p1, k = 1)$nn.dists)
+  #trace("sisterSpeciesRangeDistances", edit = T)  # 3 times min(pointDistance(p1, p2, longlat=F)
+  #to min(nn2(data = p2, query = p1, k = 1)$nn.dists)
   RD_ss <- sisterSpeciesRangeDistances(sp.rasters, sisters, aggregate_size = 2)
   
   # get sister-species-outgroup overlap metrics
@@ -222,18 +226,29 @@ for (w in 1:length(newclade.trees)){
                        ARCint        = ARCint) 
   
   results_df <- rbind(results_df, tmp_df)
+  setwd("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode")
+  write.csv(tmp_df,file=paste("EmpidatMicra", w,".csv", sep = ""))
   
- 
+ gc()
 }
 
-setwd("~/Biologia/Maestria/Tesis/official analysis/MCs-thesis/geographicmode")
-write.csv(results_df,file="Empirical_data_Micra.csv")
+tabladatos <- data.frame()
 
+for(i in 1:100) {
+  
+results <- read.csv(paste("EmpidatMicra",i,".csv", sep=""))
 
+#tabladatos <-rbind(tabladatos, results)
+}
+
+setwd("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode")
+write.csv(tabladatos,file="Empirical_data_Micra.csv")
+
+results_df <- tabladatos
 
 ### Results and figures
 
-setwd("~/Biologia/Maestria/Tesis/official analysis/MCs-thesis/geographicmode")
+setwd("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode")
 
 dat <- read.csv("simulation_results.csv",stringsAsFactors = F)
 variabs <- c("asymintercept","ROintercept","RSmean",#These are the 14 statistics used in model selection
@@ -254,21 +269,23 @@ df <- df[,2:length(df)]
 
 clados <- names(clade.trees)
 
-for (l in 1:length(cladoss)) {
-  
 
+
+for (l in 1:length(clados)) {
+  
+setwd("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode/inferencias")
+dir.create(paste("micratree", l, sep=""))
+setwd(paste("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode/inferencias/micratree",l, sep=""))
 
 #Model selection to infer the predominant geographic mode of speciation: ABC
 
-clades <- names(clados[[l]])
+clades <- clados[l]
 all.res.list <- list()
-
-setwd("~/Biologia/Maestria/Tesis/official analysis/MCs-thesis/geographicmode")
 
 for (i in 1:length(clades)){
   clade <- clades[i]
   
-  emp.dat <- read.csv(paste("./Empirical_data_",Clade,".csv",sep=""),stringsAsFactors = F)
+  emp.dat <- results_df[l,] #read.csv(paste("./Empirical_data_",Clade,".csv",sep=""),stringsAsFactors = F)
   
   colnames(emp.dat)[[which(colnames(emp.dat)=="ASYMslope")]] <- "asymslope"
   colnames(emp.dat)[[which(colnames(emp.dat)=="ASYMintercept")]] <- "asymintercept"
@@ -295,10 +312,7 @@ colnames(all.res.table)[1] <- "Mode"
 all.res.table[,1] <- as.character(all.res.table[,1])
 
 
-setwd("~/Biologia/Maestria/Tesis/official analysis/MCs-thesis/geographicmode/resultspersp")
-dir.create(names(clades))
-setwd(paste("~/Biologia/Maestria/Tesis/official analysis/MCs-thesis/geographicmode/resultspersp",
-            names(clades), sep = "/"))
+
 write.csv(all.res.table, "MicraABCresults.csv",row.names = F)
 
 all.res.table 
@@ -316,8 +330,8 @@ for (i in 1:length(clades)){
   
   
   clade <- clades[i]
-  setwd("~/Biologia/Maestria/Tesis/official analysis/MCs-thesis/geographicmode")
-  emp.dat <- read.csv(paste("./Empirical_data_",clade,".csv",sep=""),stringsAsFactors = F)
+  
+  emp.dat <- results_df[l,]#read.csv(paste("./Empirical_data_",clade,".csv",sep=""),stringsAsFactors = F)
   colnames(emp.dat)[[which(colnames(emp.dat)=="ASYMslope")]] <- "asymslope"
   colnames(emp.dat)[[which(colnames(emp.dat)=="ASYMintercept")]] <- "asymintercept"
   
@@ -327,16 +341,14 @@ for (i in 1:length(clades)){
   emp.table[i,] <- emp.dat
 }
 colnames(emp.table) <- colnames(emp.dat)
-colnames(emp.table)[[which(colnames(emp.table)=="ASYMslope")]] <- "asymslope" #
-colnames(emp.table)[[which(colnames(emp.table)=="ASYMintercept")]] <- "asymintercept" #
+#colnames(emp.table)[[which(colnames(emp.table)=="ASYMslope")]] <- "asymslope" #
+#colnames(emp.table)[[which(colnames(emp.table)=="ASYMintercept")]] <- "asymintercept" #
 emp.table <- emp.table[,which(colnames(emp.table) %in% variabs)]
 emp.table <- emp.table[,(colnames(df))]
 emp.table$models <- rep("allopatric",nrow(emp.table))
 predictions <- lda.res %>% predict(emp.table)
 lda.plot <- cbind(df.lda, predict(lda.res)$x)
 
-setwd(paste("~/Biologia/Maestria/Tesis/official analysis/MCs-thesis/geographicmode/resultspersp",
-            names(clades), sep = "/"))
 write.table(predictions,  "MicraLDA_results.txt")
 write.csv(lda.plot, "backgroundpoints.csv", row.names = F)
 
@@ -367,10 +379,6 @@ tryCatch({
   if (dev.cur() != 1) dev.off()
 })
 
-Sys.sleep(0.2)
-
-
-
 li<-predictions$x[,1:2]
 u<-as.data.frame(li)
 p<-as.data.frame(t(u))
@@ -394,3 +402,161 @@ tryCatch({
 })
 
 } 
+
+
+####  Final figures
+
+
+mafecolors<- c("#E57373","#ffff56", "#64DD17", "#6eb8f5", "#CE93D8")
+
+
+setwd("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode/inferencias")
+
+spp<-dir()
+frame <- data.frame(Gneus = spp, x.LD1 = NA, x.LD2 = NA, x.LD3 = NA, x.LD4 = NA)
+
+
+for(i in 1:length(spp)){
+  setwd(paste("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode/inferencias", spp[i], sep= "/"))
+  a<-as.data.frame(read.table(list.files(pattern = "LDA_results.txt")))
+  a<- a[1, c (7,8,9,10)]
+  frame[i, -1] <- a
+  
+}
+
+predictions <- frame
+lda.plot <- read.csv("backgroundpoints.csv")
+names(predictions)[2:3]  <- c("LD1", "LD2")
+predictions
+
+
+setwd("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode/figures")
+
+pdf("GeneralLDA.pdf", width = 10, height = 6)
+
+ggplot(lda.plot, aes(LD1, LD2)) +
+  scale_color_manual(values = mafecolors) +
+  geom_point(aes(color = models), size = 0.6) +
+  stat_ellipse(aes(color = models),level=0.95, linewidth = 2) +  
+  theme_bw() +
+  geom_point(data= predictions[,2:3], color = "black") +
+  ggtitle("LDA") +
+  xlim(-8, 8) +
+  ylim(-7.5,5)
+
+dev.off()
+
+png("GeneralLDA.png", width = 9, height = 7, units = "in", res = 600)
+ggplot(lda.plot, aes(LD1, LD2)) +
+  geom_point(aes(color = models), size = 0.6) +
+  stat_ellipse(aes(color = models),level=0.95, linewidth = 2) +  
+  theme_bw() +
+  geom_point(data= predictions[,2:3], color = "black") +
+  ggtitle("LDA")  +
+  scale_color_manual(
+    name = "Modelos",
+    values = c(
+      allopatric = "#F8766D",
+      dispersal  = "#A3A500",
+      mixed      = "#00BF7D",
+      parapatric = "#00B0F6",
+      sympatric  = "#E76BF3"
+    ),
+    labels = c(
+      allopatric = "Alopátrico",
+      dispersal  = "Peripátrico",
+      mixed      = "Mixto",
+      parapatric = "Parapátrico",
+      sympatric  = "Simpátrico"
+    )
+  )
+dev.off()
+
+
+##### COlumn bars for xomparison of best model among trees
+
+# Percentage of every geographic mode
+
+
+maximosLDA <- data.frame(Reg = NA, value = NA, Mode = NA, model = NA, Genus = NA)
+maximosmnL <- data.frame(Reg = NA, value = NA, Mode = NA, model = NA, Genus = NA)
+maximosNN <- data.frame(Reg = NA, value = NA, Mode = NA, model = NA, Genus = NA)
+
+
+for(i in 1:length(spp)){
+  setwd(paste("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode/inferencias", spp[i], sep= "/"))
+  a1<-read.csv(list.files(pattern = "Allvalues.csv"))
+  a1$Genus <- spp[i]
+  #summarydata$Group <- a1$Genus
+  
+  LDA <- a1[a1$model=="LDA",]
+  mnL <- a1[a1$model=="mnL",]
+  NN <- a1[a1$model=="NN",]
+  
+  
+  maximosLDA[i,]<- LDA[LDA$value ==max(LDA$value),]
+  maximosmnL[i,]<- mnL[mnL$value ==max(mnL$value),]
+  maximosNN[i,] <-  NN[NN$value ==max(NN$value),]
+  
+  
+}
+
+
+maximospermodel<- rbind(maximosLDA,maximosmnL,maximosNN)
+
+setwd("C:/Users/Admin/Desktop/Codigo colis/MCs-thesis/geographicmode")
+write.csv(maximospermodel, "selectedModePerModel.csv", row.names = F)
+
+png("barplotfreq.png", width = 9, height = 5, units = "in", res = 600)
+
+ggplot(maximospermodel, aes(Mode, fill = Mode)) +
+  geom_bar() +
+  xlab("") +
+  ylab("Frecuencia") +
+   facet_wrap(~model) +
+   theme_classic() +
+  theme(
+    panel.border = element_rect(color = "black", fill = NA, linewidth = 0.8),
+    strip.background = element_rect(color = "black", fill = "white"),
+    axis.text.x = element_blank(),
+    axis.ticks.x = element_blank(),
+    strip.text = element_text(size = 16, face = "bold")
+  ) +
+  scale_fill_manual(
+    values = c(
+      allopatric = "#F8766D",
+      dispersal  = "#A3A500",
+      mixed      = "#00BF7D",
+      parapatric = "#00B0F6",
+      sympatric  = "#E76BF3"
+    ),
+    labels = c(
+      allopatric = "Alopátrico",
+      dispersal  = "Peripátrico",
+      mixed      = "Mixto",
+      parapatric = "Parapátrico",
+      sympatric  = "Simpátrico"
+    ),
+    name = "Modo"
+  )
+  
+dev.off()
+
+
+modes <- c("allopatric", "dispersal" , "mixed", "parapatric", "sympatric" )
+
+modos <- data.frame (Modes = modes)
+
+# conteo en todos
+
+allxLDA<-as.data.frame(table(maximosLDA$Mode))
+allxmnL<-as.data.frame(table(maximosmnL$Mode))
+allxNN<-as.data.frame(table(maximosNN$Mode))
+
+names(allxLDA)<- c("Modes", "Value")
+names(allxmnL)<- c("Modes", "Value")
+names(allxNN)<- c("Modes", "Value")
+
+
+
+
